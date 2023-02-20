0b1055ab27a1345308353ac05d8969d70bf29ac0
Svitlana <90215652+SvitlanaMa@users.noreply.github.com>
2023-02-20 20:42:49 +0200

    Update and rename main.py to main1.py

--- main1.py
+++ main1.py
@@ -6,11 +6,7 @@ spark = SparkSession.builder.appName("air").getOrCreate()
 
 df_2007 = spark.read.option("header", True).csv("2007.csv.bz2")
 df_carr = spark.read.option("header", True).csv("carriers.csv")
-# df_airp = spark.read.option("header", True).csv("airports.csv")
 
-# df_2007.printSchema()
-# df_2007.show(5)
 df_res1 = df_2007.where(df_2007.Year=="2007").groupBy("UniqueCarrier").count()
 df_res2 = df_res1.join(df_carr, df_res1.UniqueCarrier == df_carr.Code).select("Code", sf.col("Description").alias("Carrier"), sf.col("count").alias("Total_flights"))
 df_res2.show()
-# print(df_res1.columns)
